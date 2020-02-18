9717128a41131c7a18bd21a140b8d0fed1377226
erik-cf <erikcf95@gmail.com>
2020-02-18 10:15:53 +0100

    tret el listen que fallava

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+//app.listen(3000, () => console.log('Example app listening on port 3000!'));
